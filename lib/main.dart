22941e5bd191f60605e9806774091e92cd096aa5
Amaldas467 <amaldas467@gmail.com>
2024-02-22 09:09:56 +0530

    mainfile updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:note_application/view/splash_screen/splash_screen.dart';
 
 void main() {
-  runApp(NoteApp());
+  runApp(MyApp());
 }
 
-class NoteApp extends StatelessWidget {
-  const NoteApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
